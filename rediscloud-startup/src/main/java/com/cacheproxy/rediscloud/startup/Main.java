6dd49128482dcd4d2c1763888728db4d5e1bf4f1
serversocket <liyajishu@163.com>
2017-05-09 13:55:52 +0800

    换了 邮箱继续提交

--- rediscloud-startup/src/main/java/com/cacheproxy/rediscloud/startup/Main.java
+++ rediscloud-startup/src/main/java/com/cacheproxy/rediscloud/startup/Main.java
@@ -11,6 +11,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		
 		new ClassPathXmlApplicationContext(new String[] { "classpath*:redisCloud.xml" });
+		
 	}
 }
